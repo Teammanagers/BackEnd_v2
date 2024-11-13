bfb013d17a5941b7bea0330119e54cd4c5ebc1a5
Luke_YS <yjsmk0902@gmail.com>
2024-11-13 11:41:36 +0900

    :tada: chore: Set up

---
+++ src/test/java/kr/teammangers/dev/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.teammangers.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
