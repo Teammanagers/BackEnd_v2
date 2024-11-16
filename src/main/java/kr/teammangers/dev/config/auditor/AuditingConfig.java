1d4efbcdf97b034bb00898f4dbd5be95cb5990ef
Luke_YS <yjsmk0902@gmail.com>
2024-11-16 14:51:06 +0900

    :wrench: (TM-21) Chore: Audit 세팅

---
+++ src/main/java/kr/teammangers/dev/config/auditor/AuditingConfig.java
@@ -0,0 +1,23 @@
+package kr.teammangers.dev.config.auditor;
+
+import kr.teammangers.dev.member.domain.Member;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+public class AuditingConfig {
+
+    public AuditorAware<Long> auditorProvider() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
+            return Optional.of(((Member) authentication.getPrincipal()).getId());
+        };
+    }
+}
