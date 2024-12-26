96ad2033a5c2632d6901bb512226839a64540338
Luke_YS <yjsmk0902@gmail.com>
2024-12-26 16:37:55 +0900

    :recycle: [TM-114] Refactor: 태그 관련 API CUD 응답 통합

--- src/main/java/kr/teammangers/dev/tag/dto/request/CreateTeamMemberTagReq.java
+++ src/main/java/kr/teammangers/dev/tag/dto/request/CreateTeamMemberTagReq.java
@@ -1,8 +1,6 @@
 package kr.teammangers.dev.tag.dto.request;
 
 public record CreateTeamMemberTagReq(
-        Long memberId,
-        Long teamId,
         String tagName
 ) {
 }
--- src/main/java/kr/teammangers/dev/tag/mapper/TeamMemberTagMapper.java
+++ src/main/java/kr/teammangers/dev/tag/mapper/TeamMemberTagMapper.java
@@ -2,6 +2,7 @@ package kr.teammangers.dev.tag.mapper;
 
 import kr.teammangers.dev.tag.domain.entity.Tag;
 import kr.teammangers.dev.tag.domain.entity.TeamMemberTag;
+import kr.teammangers.dev.tag.dto.TeamMemberTagDto;
 import kr.teammangers.dev.team.domain.entity.TeamMember;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -16,4 +17,8 @@ public interface TeamMemberTagMapper {
     @Mapping(target = "teamMember", source = "teamMember")
     @Mapping(target = "tag", source = "tag")
     TeamMemberTag toEntity(TeamMember teamMember, Tag tag);
+
+    @Mapping(target = "tagId", source = "teamMemberTag.tag.id")
+    @Mapping(target = "teamMemberId", source = "teamMemberTag.teamMemberTag.id")
+    TeamMemberTagDto toDto(TeamMemberTag teamMemberTag);
 }
