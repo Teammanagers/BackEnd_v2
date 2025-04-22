bb458c0ea5d152521683194663d4a1b94b044268
lheesoo1020 <lheesoo1020@gmail.com>
2025-04-22 13:16:40 +0900

    fix: todo 삭제 검증 오류 수정

--- src/main/java/kr/teammangers/dev/todo/application/TodoCrudService.java
+++ src/main/java/kr/teammangers/dev/todo/application/TodoCrudService.java
@@ -60,7 +60,7 @@ public class TodoCrudService {
         Todo todoForDelete = todoRepository.findById(todoId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.TODO_NOT_FOUND));
 
-        if (!teamMemberRepository.existsByTeam_IdAndMember_Id(memberId, todoForDelete.getTeamMember().getTeam().getId())) {
+        if (!teamMemberRepository.existsByTeam_IdAndMember_Id(todoForDelete.getTeamMember().getTeam().getId(), memberId)) {
             throw new GeneralException(ErrorStatus.TODO_FORBIDDEN);
         }
 
