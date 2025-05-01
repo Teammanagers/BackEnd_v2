c4487ec0eb9ae70356b69b0075fa5b0984b36716
Boreum <itip1@mju.ac.kr>
2025-05-01 17:25:27 +0900

    fix: requestMahers add

--- src/main/java/kr/teammangers/dev/global/config/security/SecurityConfig.java
+++ src/main/java/kr/teammangers/dev/global/config/security/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PUBLIC_URLS).permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
