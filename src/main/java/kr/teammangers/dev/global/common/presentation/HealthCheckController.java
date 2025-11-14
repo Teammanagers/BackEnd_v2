6e580def62930fcc194cb3162c7e716cb6cf6c76
Luke_YS <52543606+yjsmk0902@users.noreply.github.com>
2025-11-14 09:36:02 +0900

    refactor: Add package info

--- src/main/java/kr/teammangers/dev/global/common/presentation/HealthCheckController.java
+++ src/main/java/kr/teammangers/dev/global/common/presentation/HealthCheckController.java
@@ -1,3 +1,5 @@
+package kr.teammangers.dev.global.common.presentation;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
